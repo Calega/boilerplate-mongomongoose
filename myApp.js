25ded2564ec7a3c31662720db40d150230a5b9e8
lucascalegari <lcalegari17@gmail.com>
2019-01-08 19:00:19 -0800

    removing by object name

--- myApp.js
+++ myApp.js
@@ -257,8 +257,10 @@ var removeById = function(personId, done) {
 
 var removeManyPeople = function(done) {
   var nameToRemove = "Mary";
-
-  done(null/*, data*/);
+  
+  Person.remove( { name: nameToRemove }, function(err,data) {
+    err ? done(err) : done(null,data);
+  });
 };
 
 /** # C[R]UD part V -  More about Queries # 
